3fbf7e42972b2be291b0401f3d24309d820e97c6
J.C. Hiatt <jchiatt@me.com>
2017-05-08 13:44:09 -0500

    Add loading feedback to contact form

--- static/contactForm.js
+++ static/contactForm.js
@@ -6,16 +6,17 @@ var contactForm = function() {
         '<p class="rnt-contact-form-text">Use our form below, our email us directly:<br><span><a href=\"mailto:info@reactnative.training\">info@reactnative.training</a></span></p>',
         '<div class="rnt-contact-form-fields">',
         '<select name="inquiry-type" id="rnt-inquiry-type" required>',
-            '<option disabled selected value="">What\'s this about?</option>',
-            '<option value="Corporate Training">Corporate Training</option>',
-            '<option value="Workshops">Workshops</option>',
-            '<option value="General Inquiry">General Inquiry</option>',
+            '<option disabled selected value="">what\'s this about?</option>',
+            '<option value="Corporate Training">corporate training</option>',
+            '<option value="Workshops">workshops</option>',
+            '<option value="General Inquiry">general inquiry</option>',
           '</select>',
-          '<input type="text" name="name" placeholder="Your name" required>',
-          '<input type="text" name="_replyto" placeholder="Your email" required>',
-          '<textarea name="rnt-contact-message" placeholder="Type your message here"></textarea>',
+          '<input type="text" name="name" placeholder="your name" required>',
+          '<input type="text" name="_replyto" placeholder="your email" required>',
+          '<textarea name="rnt-contact-message" placeholder="type your message here"></textarea>',
           '<input type="hidden" name="_subject" value="New contact form submission">',
           '<input type="text" name="_gotcha" style="display:none">',
+          '<img src=../static/react-logo.svg class="react-spinner">',
           '<!--<input class="rnt-contact-submit" type="submit" value="Send">-->',
         '</div>'
     ].join(''),
@@ -23,12 +24,21 @@ var contactForm = function() {
       if (!data) {
         return console.log('Cancelled')
       }
+      $('.react-spinner').show();
       $.ajax({
         url: "https://formspree.io/xqekropx", 
         method: "POST",
         data: data,
-        dataType: "json"
-      });
+        dataType: "json",
+        success: function() {
+          vex.dialog.open({
+            message: 'Message sent. Thanks!',
+            buttons: [
+              $.extend({}, vex.dialog.buttons.YES, { text: 'Close' }),
+            ]
+          })
+        }
+      })
     }
   })
 }
